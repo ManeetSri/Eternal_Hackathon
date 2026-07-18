69c6af3dfaa3c24d99352f3c0969493139aaa64a
Maneet Srivastav <srivastavmaneet@gmail.com>
2026-07-18 15:12:22 +0530

    Initial commit

---
+++ Eternal Scan/Eternal_ScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eternal_ScanApp.swift
+//  Eternal Scan
+//
+//  Created by Maneet@MLL on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Eternal_ScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
